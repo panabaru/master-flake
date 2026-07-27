7e2534a40ec7fb85e1da4583f25e8a56dc0b8541
panabaru <graintrain@keemail.me>
2026-07-26 20:16:29 -0400

    redid minecraft.nix

--- common/graphical.nix
+++ common/graphical.nix
@@ -34,6 +34,9 @@
   services.pipewire = {
     enable = true;
     pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    wireplumber.enable = true;
   };
  # Printing
   services.printing.enable = true;
--- hosts/server/minecraft.nix
+++ hosts/server/minecraft.nix
@@ -8,32 +8,57 @@
     eula = true;
     openFirewall = true;
     dataDir = "/srv/minecraft";
-  
 
-    servers.modded = {
-      enable = true;
-      autoStart = true;
+    servers.modded =
+      let
+        # Vanilla Perfected, build "Tiny Takeover Hotfix 3.0" (1.0.3+26.1.2),
+        # the current release for Minecraft 26.1.2 / Fabric as of July 2026.
+        # https://modrinth.com/modpack/vanilla-perfected/version/1.0.3+26.1.2
+        modpack = pkgs.fetchModrinthModpack {
+          url = "https://cdn.modrinth.com/data/1ocGzRHv/versions/zCNpmrT6/Vanilla%20Perfected%201.0.3%2B26.1.2.mrpack";
+          # First build will fail with a hash mismatch and print the real
+          # value to use here - copy that in and rebuild. See note below.
+          packHash = pkgs.lib.fakeHash;
+          side = "server";
+        };
+      in
+      {
+        enable = true;
+        autoStart = true;
 
-      package = pkgs.fabricServers.fabric-26_1_2.override {
-        jre_headless = pkgs.temurin-jre-bin-25;
-        loaderVersion = "0.19.2";
+        package = pkgs.fabricServers.fabric-26_1_2.override {
+          jre_headless = pkgs.temurin-jre-bin-25;
+          loaderVersion = "0.19.2";
+        };
+        jvmOpts = "-Xms2G -Xmx8G";
+
+        # This is what was missing: without it, the server has the Fabric
+        # loader but none of Vanilla Perfected's actual mods installed.
+        symlinks = {
+          "mods" = "${modpack}/mods";
+        };
+        files = {
+          "config" = "${modpack}/config";
+        };
+
+        serverProperties = {
+          server-port = 25565;
+          difficulty = 3;
+          gamemode = 0;
+          max-players = 3;
+          motd = "Vanilla Prrrfected";
+          white-list = false;
+          enable-rcon = true;
+          # Was "rcon-password" (hyphen) - Minecraft only recognizes the
+          # dotted key below. With the hyphen, this line was written into
+          # server.properties but silently ignored, leaving RCON's real
+          # password empty.
+          "rcon.password" = "@rcon_password@";
+        };
+
+        operators = {
+          graintrain = "168a0ea4-556b-47e8-b454-9df60a62cbc9";
+        };
       };
-      jvmOpts = "-Xms2G -Xmx8G";
-
-      serverProperties = {
-        server-port = 25565;
-        difficulty = 3;
-        gamemode = 0;
-        max-players = 3;
-        motd = "Vanilla Prrrfected";
-        white-list = false;
-        enable-rcon = true;
-        "rcon.password" = "@rcon_password@";
-      };
-
-      operators = {
-        graintrain = "168a0ea4-556b-47e8-b454-9df60a62cbc9";
-      };
-    };
   };
 }
